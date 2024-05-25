9429f660b2d516cee47279e427deb6656405f6a4
Tanafilo <study.ryannguyen@gmail.com>
2024-05-25 09:53:59 +0700

    update

---
+++ Caro/__init__.py
@@ -0,0 +1,31 @@
+import pygame
+
+import random
+
+pygame.init()
+
+class Menu:
+    def UI:
+
+    def load_game:
+            
+    def time_set:
+
+    def time_display:
+
+    def setting:
+    '''
+        draw, forfeit, pause
+    '''
+    
+    def end_game:
+
+class Board:
+    def __init__(self):
+
+    def generate:
+
+class Player:
+    def __init__(self, number):
+        
+
---
+++ Caro/config.py
@@ -0,0 +1,5 @@
+# Configurations
+
+Screen_width = 
+
+Screen_height = 
